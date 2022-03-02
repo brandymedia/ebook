9cf658282c372cbb0decdcfb2d8bf965eed3ecc8
Andy Griffiths <andygriffiths@Andys-MacBook-Pro.local>
2022-03-02 09:40:52 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+(function () {
+    document.querySelector(".copyright").innerHTML = `Copyright © ${ new Date().getFullYear() } <a target="_blank" class="underline" href="https://www.brandymedia.co.uk/">Brandy Media</a>`;
+})();
